238aa3248bc8bd5f1be3d75eb3645c27722a0225
paulpaulchan <116852901+ngwingyan@users.noreply.github.com>
2023-03-22 19:31:09 +0000

    updated drag and drop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import TodoList from "./components/TodoList";
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+// import {DndContext} from '@dnd-kit/core';
+// import {Draggable} from '@dnd-kit/Draggable';
+// import {Droppable} from '@dnd-kit/Droppable';
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <DndProvider backend={HTML5Backend}>
+    <div className="todo-app">
+
+        <TodoList />
+      
     </div>
+    </DndProvider>
   );
 }
+
+
 
 export default App;
